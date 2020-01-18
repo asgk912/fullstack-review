99f2b139c47882b6c93505e6eea985fcc010c992
Albert <asgk912@gmail.com>
2020-01-18 11:25:45 -0800

    update schema of Repo table and complete save function in database/index.js

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Search from './Search.jsx';
 
-const RepoList = (props) => (
+const RepoList = (props) => {
+  // var list = props.repos.map((repo) => (<div className="repo">Forks:{repo.forks}, Username:{repo.username}, Reponame:{repo.reponame}</div>));
+  return(
   <div>
     <h4> Repo List Component </h4>
     There are {props.repos.length} repos.
+    {props.repos.map((repo, index) => (<div className="repo" key={index.toString()}>Created Date: {repo.created_at.toString().substring(4,15)}, Username: {repo.username}, Reponame: {repo.reponame}, Forks: {repo.forks}</div>))}
   </div>
-)
+  );
+}
 
 export default RepoList;
--- database/index.js
+++ database/index.js
@@ -1,21 +1,59 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher',{useMongoClient: true});
+// ********************************************************************
+// ADDuseFindAndModify option
+
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  created_at: Date,
   username: String,
   reponame: String,
-  forks: Number,
+  forks: Number
 });
 
-repoSchema.index({forks:1, username:1});
+repoSchema.index({created_at:1, username:1});
+
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repos) => {
+  /************************************************
+      CONSIDER callback function as 2nd argument of save
+              and invoke callback on error/success 
+  ************************************************/
+
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
+  repos.map((repo) => {
+    Repo.findOne({created_at: repo.created_at, username: repo.owner.login, reponame: repo.name}, (err, foundRepo) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if(foundRepo) {
+        // repo is found, update its forks number
+          foundRepo.update({forks: repo.forks}, (err, raw) => {
+            if(err) {
+              console.log(err);
+            } else {
+              console.log(raw);
+            }
+          });  
+        } else {
+          var repoDoc = new Repo({created_at: repo.created_at, username: repo.owner.login, reponame: repo.name, forks: repo.forks});
+          repoDoc.save( (err) => {
+            if(err){
+              console.log(err);
+            } else {
+              console.log('new repo saved successfully');
+            }
+          });
+        }
+      }
+    });
+  });
 }
 
 module.exports.save = save;
