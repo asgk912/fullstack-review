fbca3d7bb2f7ca7c41076b1a3f5704ce6c7aee91
Albert <asgk912@gmail.com>
2020-01-17 19:40:51 -0800

    add loggina and parsing features and simple response to post request

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+
 let app = express();
 
+// logging & parsing
+const morgan = require('morgan');
+app.use(morgan('dev'));
+app.use(express.urlencoded());
+
+// define path for static files
 app.use(express.static(__dirname + '/../client/dist'));
 
+// Handle HTTP requests
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  res.sendStatus(200);
 });
 
 app.get('/repos', function (req, res) {
